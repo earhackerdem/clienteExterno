fd66f758ea3792b2afeae192eeebf6fccfd12ca6
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-01-13 15:00:06 -0600

    51 - Solicitar AccessToken

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 
 class OauthController extends Controller
@@ -24,6 +25,21 @@ class OauthController extends Controller
 
     public function callback(Request $request)
     {
-        return $request->all();
+        $state = $request->session()->pull('state');
+
+        throw_unless(
+            strlen($state) > 0 && $state === $request->state,
+            InvalidArgumentException::class
+        );
+
+        $response = Http::asForm()->post('http://127.0.0.1:8000/oauth/token', [
+            'grant_type' => 'authorization_code',
+            'client_id' => config('services.api.client_id'),
+            'client_secret' => config('services.api.client_secret'),
+            'redirect_uri' => route('callback'),
+            'code' => $request->code,
+        ]);
+
+        return $response->json();
     }
 }
